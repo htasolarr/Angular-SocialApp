fd1f3d9fbf6650004497edf7bc5241abbc426f14
htasolarr <hilaltasolar@gmail.com>
2021-08-09 18:13:44 +0300

    initial

---
+++ ServerApp/DTO/UserForRegisterDTO.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace ServerApp.DTO
+{
+    public class UserForRegisterDTO
+    {
+        [Required(ErrorMessage="name gerekli bir alan.")]  
+        [StringLength(50,MinimumLength=10)]
+        public string Name { get; set; }
+        [Required] 
+        public string UserName { get; set; }
+        [Required] 
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        [Required] 
+        public string Password { get; set; }
+        [Required] 
+        public DateTime DateOfBirth { get; set; }
+         [Required] 
+        public string City { get; set; }
+        [Required] 
+        public string Country { get; set; }
+}
+}
